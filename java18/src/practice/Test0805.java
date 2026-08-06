afeea8538b6835404cd640cde026fbcae25d8117
min <cat4367@naver.com>
2026-08-06 16:52:30 +0900

    fail

--- java18/src/practice/Test0805.java
+++ java18/src/practice/Test0805.java
@@ -1,34 +1,83 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test0805 {
 
 	public static void main(String[] args) {
 		
-		int result = solution(5,7);
-		System.out.println(result);
+//		int result = solution(5,7);
+//		System.out.println(result);
 
-	}
-	
-	
-	static int solution(int a, int b) {
-		int result = 0;
-		int[] arr1 = new int[15];
-		int[] arr2 = new int[15];
 		
-		arr1[0] = a;
-		arr2[0]	= b;
+		int[] list = {2,6,8,14,20,3};
 		
-		for(int i = 1; i <= arr1.length; i++) {
-			for(int j = 1; j<= arr1.length; j++) {
-				if(arr1[0]*i == arr2[0]*j) {
-					result = arr1[0]*i;
-					return result;
+		while(list.length >= 1) {
+			for(int i = 1; i <= list[0]; i++) {
 				
-				}
 			}
-			
 		}
-		return result;
+		
+		
+//		Map<Integer, ArrayList<Integer>> sam = new HashMap<>();
+//		
+//		for(int i = 0; i < list.length; i++) {
+//			ArrayList<Integer> sam2 = new ArrayList<>();		
+//			for(int j = 1; j <= list[i]; j++) {
+//				if(list[i]%j==0) {
+//					sam2.add(j);
+//					
+//				}
+//			}
+//			sam.put(i, sam2);
+//		}
+//		System.out.println(sam.get(0));
+//		System.out.println(sam.get(1));
+//		
+//		for(int i = 1; i < list.length; i++) {
+//			sam.get(0).retainAll(sam.get(i));
+//		}
+//		System.out.println(sam.get(0));
+//		int maxSam = Collections.max(sam.get(0));
+//		System.out.println(maxSam);
+//		int[] bai = new int[list.length];
+//		for(int i = 0; i < list.length; i++ ) {
+//			bai[i] = list[i]/maxSam;
+//		}
+//		int last = 1;
+//		for(int i = 0; i < list.length; i++) {
+//			last *= bai[i];
+//		}
+//		System.out.println(last);
 	}
+	
+	
+	
+//	static int solution(int a, int b) {
+//		int result = 0;
+//		int[] arr1 = new int[15];
+//		int[] arr2 = new int[15];
+//		
+//		arr1[0] = a;
+//		arr2[0]	= b;
+//		
+//		for(int i = 1; i <= arr1.length; i++) {
+//			for(int j = 1; j<= arr1.length; j++) {
+//				if(arr1[0]*i == arr2[0]*j) {
+//					result = arr1[0]*i;
+//					return result;
+//				
+//				}
+//			}
+//			
+//		}
+//		return result;
+//	}
+	
+	
 
 }
